5aeb674911a2f6a030b34c530ed0a3edb30eb0a5
Ardra-14 <144412428+Ardra-14@users.noreply.github.com>
2024-04-29 13:18:28 +0530

    Add files via upload

---
+++ Merging.java
@@ -0,0 +1,15 @@
+public class Merging {
+    public  int[] combinedArrays(int[] arr1, int[] arr2){
+        int[] combinedArray = new int[arr1.length + arr2.length];
+        int j = 0;
+        for (int i = 0; i < arr1.length; i++){
+            combinedArray[j] = arr1[i];
+            j++;
+        }
+        for (int i = 0; i < arr2.length; i++){
+            combinedArray[j] = arr2[i];
+            j++;
+        }
+        return combinedArray;
+    }
+}
